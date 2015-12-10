6cde7eb4b9d8fd0cc7740628862f63ec8bb6345b
Risto Rautio <rautio.risto@gmail.com>
2015-12-10 20:59:50 +0200

    raspi  working

--- UniversalClient/MainPage.xaml.cs
+++ UniversalClient/MainPage.xaml.cs
@@ -13,6 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.AspNet.SignalR.Client;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client.Transports;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,8 +29,47 @@ namespace UniversalClient
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.Loaded += MainPage_Loaded;
         }
 
-        
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await SetupHub();
+        }
+
+        private async Task SetupHub()
+        {
+            //http://raudio.azurewebsites.net/api/Lamp/TurnLamp?turnOn=true
+
+            var hubconnection = new HubConnection("http://raudio.azurewebsites.net/");
+            var lamphub = hubconnection.CreateHubProxy("Lamp");
+            lamphub.On<bool>("Turn", handleCallback);
+            await hubconnection.Start(new LongPollingTransport());
+        }
+
+        private void handleCallback(bool state)
+        {
+            SetStatus(state);
+        }
+
+        private void SetStatus(bool state)
+        {
+
+
+            Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+    () =>
+    {
+        b1.Content = state.ToString();
+    });
+
+            //if (button1.InvokeRequired)
+            //{
+            //    button1.Invoke(new Action<bool>(SetStatus), state);
+            //}
+
+            //button1.Text = state.ToString();
+        }
+
     }
 }
