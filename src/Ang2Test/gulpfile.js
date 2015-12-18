02a42af33d250b4b836988177b9947891ddf9f35
Risto Rautio <rautio.risto@gmail.com>
2015-12-18 10:47:31 +0200

    hero working

--- src/Ang2Test/gulpfile.js
+++ src/Ang2Test/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding  AfterBuild='apps' Clean='clean' />
+﻿/// <binding AfterBuild='apps, libs' Clean='clean' />
 
 var gulp = require('gulp');
 var rimraf = require('rimraf');
@@ -10,19 +10,24 @@ var paths = {
 };
 
 var libs = [
-    paths.npm+"angular2/bundles/angular2.dev.js",
-    paths.npm + "es6-shimm/es6-shim.js",
-    paths.npm + "systemjs/dist/system.js"
+    paths.npm + 'angular2/bundles/angular2.dev.js',
+    paths.npm + 'angular2/bundles/angular2-polyfills.js',
+    paths.npm + 'es6-shim/es6-shim.js',
+    paths.npm + 'systemjs/dist/system.src.js',
+    paths.npm + 'systemjs/dist/system-polyfills.js',
+    paths.npm + 'rxjs/bundles/Rx.js',
 ];
+
 
 var apps = [
     "scripts/app/**/*.js"
 ];
 
 
-gulp.task("libs", function () {
+gulp.task('libs', function () {
     return gulp.src(libs).pipe(gulp.dest(paths.lib));
 });
+
 
 gulp.task("apps", function () {
     return gulp.src(apps).pipe(gulp.dest(paths.app));
